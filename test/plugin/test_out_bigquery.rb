48da9c87553bb8a7f95eafe3b3d6c191a802f436
yoheimuta <yoheimuta@gmail.com>
2017-03-16 14:52:40 +0900

    Retried the insert api with 504 error

--- test/plugin/test_out_bigquery.rb
+++ test/plugin/test_out_bigquery.rb
@@ -783,6 +783,7 @@ class BigQueryOutputTest < Test::Unit::TestCase
       { "status_code" => 500  },
       { "status_code" => 502  },
       { "status_code" => 503  },
+      { "status_code" => 504  },
     ]
 
     data_input.each do |d|
